495ab2e97587ca866a898b8a13a2637903ffc45b
4hauntzer <igorcardosod3@gmail.com>
2020-03-07 17:21:51 -0300

    Navbar Part2

--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -26,16 +26,13 @@ const Header = () => {
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse nav justify-content-end" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Serviços</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contatos</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Contatos</a>
