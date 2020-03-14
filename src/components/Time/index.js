7ad601d8a5a00f76c730f1381622e854c3a36601
4hauntzer <igorcardosod3@gmail.com>
2020-03-14 16:27:50 -0300

    nossoTimeFeito

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -15,11 +15,13 @@ const Time = () => (
             <div className="time_text">
                <img className="time_image" src={image} />
                   <p className="time__text ">
-                     Jessica Mazonini
+                     Jessica Mazoni
                   </p>
                   <p className="time__text ">
-                     Presidente(a)
+                     Presidenta
                   </p>
+                  <a href="INSTAGRAM"><i class="fab fa-instagram time_instF"></i></a>
+                     <a href="LINKEDIN AQ "><i class="fab fa-linkedin time_instF "></i></a>
                
             </div>
                <div className="time_text">
@@ -27,24 +29,44 @@ const Time = () => (
                      <p className="time__text ">
                                        Jonas Salatiel
                      </p>
+                     <p className="time__text ">
+                        NDP
+                     </p>
+                     <a href="https://www.instagram.com/jonassalatiel/?hl=pt-br"><i class="fab fa-instagram time_instF"></i></a>
+                     <a href="https://www.linkedin.com/in/jonas-salatiel/"><i class="fab fa-linkedin time_instF "></i></a>
                </div>
                <div className="time_text">
                   <img className="time_image" src={image} />
                   <p className="time__text">
                                        Victor
                   </p>
+                  <p className="time__text ">
+                        NAF
+                  </p>
+                  <a href="INSTAGRAM"><i class="fab fa-instagram time_instF"></i></a>
+                     <a href="LINKEDIN AQ "><i class="fab fa-linkedin time_instF "></i></a>
                </div>
                <div className="time_text">
                   <img className="time_image" src={image} />
                   <p className="time__text ">
                                        Aline Lopes
                   </p>
+                  <p className="time__text ">
+                        NGP
+                     </p>
+                     <a href="INSTAGRAM"><i class="fab fa-instagram time_instF"></i></a>
+                     <a href="LINKEDIN AQ "><i class="fab fa-linkedin time_instF "></i></a>
                </div>
                <div className="time_text">
                   <img className="time_image" src={image} />
                      <p className="time__text ">
                                        Carlos Henrique
                      </p>
+                     <p className="time__text ">
+                        NCM
+                     </p>
+                     <a href="INSTAGRAM"><i class="fab fa-instagram time_instF"></i></a>
+                     <a href="LINKEDIN AQ "><i class="fab fa-linkedin time_instF "></i></a>
                </div>
 
          </div>
